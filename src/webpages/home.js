ea2a9cefa3c0565fb87fd1d8f4bc8953c9871266
Kevin <kevinnguyen90.kr@gmail.com>
2024-06-10 00:50:50 +0900

    update 2024061000

--- src/webpages/home.js
+++ src/webpages/home.js
@@ -1,9 +1,9 @@
 //file: src/webpages/home.js
 import React from 'react';
 import jor115116 from '../images/Bia/Bia so 115116.jpg';
-import jor114 from '../images/Bia/Bia so 114.jpg';
-import jor113 from '../images/Bia/Bia so 113.jpg';
-import jor112 from '../images/Bia/Bia so 112.jpg';
+import jor119 from '../images/Bia/Bia so 119.jpg';
+import jor117 from '../images/Bia/Bia so 117.jpg';
+import jor118 from '../images/Bia/Bia so 118.jpg';
 
 import { Link } from 'react-router-dom';
 
@@ -14,35 +14,35 @@ const Home = () => {
         <div>
             <li>
             <li class="service-list">
+                <Link to={{pathname: '/journalpage', state:'119'}}><img src={jor119} height="200"  /></Link>
+
+                <h3>Tạp chí Rừng và Môi trường số 119</h3>
+                <p>
+                Mời Quý đọc giả đón đọc những bài báo nghiên cứu, những thông tin hữu ích về rừng và môi trường số 119, năm 2023.
+                </p>
+            </li>
+            <li class="service-list">
+                <Link to={{pathname: '/journalpage', state:'118'}}><img src={jor118} height="200"  /></Link>
+
+                <h3>Tạp chí Rừng và Môi trường số 118</h3>
+                <p>
+                Mời Quý đọc giả đón đọc những bài báo nghiên cứu, những thông tin hữu ích về rừng và môi trường số 118, năm 2023.
+                </p>
+            </li>
+            <li class="service-list">
+                <Link to={{pathname: '/journalpage', state:'117'}}><img src={jor117} height="200"  /></Link>
+
+                <h3>Tạp chí Rừng và Môi trường số 117</h3>
+                <p>
+                Mời Quý đọc giả đón đọc những bài báo nghiên cứu, những thông tin hữu ích về rừng và môi trường số 117, năm 2023.            </p>
+            </li>
+
+            <li class="service-list">
                 <Link to={{pathname: '/journalpage', state:'115116'}}><img src={jor115116} height="200"  /></Link>
 
                 <h3>Tạp chí Rừng và Môi trường số 115 và 116</h3>
                 <p>
-                Tạp chí Rừng và Môi trường ra số gộp gồm số 115 và 116. 
-                </p>
-            </li>
-            <li class="service-list">
-                <Link to={{pathname: '/journalpage', state:'114'}}><img src={jor114} height="200"  /></Link>
-
-                <h3>Tạp chí Rừng và Môi trường số 114</h3>
-                <p>
-                Tạp chí Rừng và Môi trường ra số 114. 
-                </p>
-            </li>
-            <li class="service-list">
-                <Link to={{pathname: '/journalpage', state:'113'}}><img src={jor113} height="200"  /></Link>
-
-                <h3>Tạp chí Rừng và Môi trường số 113</h3>
-                <p>
-                Mời Quý đọc giả đón đọc những bài báo nghiên cứu, những thông tin hữu ích về rừng và môi trường số 113, năm 2022.            </p>
-            </li>
-
-            <li class="service-list">
-                <Link to={{pathname: '/journalpage', state:'112'}}><img src={jor112} height="200"  /></Link>
-
-                <h3>Tạp chí Rừng và Môi trường số 112</h3>
-                <p>
-                Tạp chí Rừng và Môi trường trân trọng cảm ơn các tác giả đã gửi bài đăng trong các số 112
+                Tạp chí Rừng và Môi trường trân trọng cảm ơn các tác giả đã gửi bài đăng trong các số 115 và 116
                 </p>
             </li>
             </li>
